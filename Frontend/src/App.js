dd7b65f875e06a1e965fd7ee08d1eb9ced4a6252
Jevgenij <sssgustavsss@gmail.com>
2023-03-05 10:15:15 +0200

    add frontent

---
+++ Frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navigation from './components/navigation/Navigation.jsx';
+import Registration from './components/registration/Registration.jsx';
+
+const App = () =>
+{
+  return (
+    <div>
+      <Navigation />
+      <Registration />
+    </div>
+  )
+}
+
+export default App;
