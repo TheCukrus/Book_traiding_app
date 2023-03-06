e48e62ba7bd79b39f74d0b13d35e3874d254e7ff
Jevgenij <sssgustavsss@gmail.com>
2023-03-06 22:31:19 +0200

    updates in navigation

--- Frontend/src/components/navigation/Navigation.jsx
+++ Frontend/src/components/navigation/Navigation.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import s from "./Navigation.module.css";
 
 const Navigation = () =>
 {
     return (
-        <div>
+        <div className={s.container}>
             <Link to="/">Logo</Link>
-            <Link to="/registration">Registration</Link>
-            <h1>Logo</h1>
             <input type="search" placeholder="Search for books" />
             <h1>profile</h1>
-            <div>
-                {/*Meniu items*/}
-                <h1>Home</h1>
-                <h1>Profile</h1>
-                <h1>About</h1>
-                <h1>Contact</h1>
-            </div>
+            <Link to="/registration">Registration</Link>
+
         </div>
 
     )
