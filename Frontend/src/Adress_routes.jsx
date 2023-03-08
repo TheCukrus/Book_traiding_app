367fd1f6357dde150c86929b28fc85f991290160
Jevgenij <sssgustavsss@gmail.com>
2023-03-08 23:09:56 +0200

    add success message to route

--- Frontend/src/Adress_routes.jsx
+++ Frontend/src/Adress_routes.jsx
@@ -7,7 +7,7 @@ const Adress_routes = (props) =>
     return (
         <Routes>
             <Route exact path="/" element={<Home />} />
-            <Route path="/registration" element={<Registration error={props.error} set_error={props.set_error} />} />
+            <Route path="/registration" element={<Registration error={props.error} set_error={props.set_error} success_message={props.success_message} set_success_message={props.set_success_message} />} />
         </Routes>
     )
 }
