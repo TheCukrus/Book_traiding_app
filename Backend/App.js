96284ee2aabc5500857eac6139e49b2967af132e
Jevgenij <sssgustavsss@gmail.com>
2023-03-13 05:20:18 +0200

    modify app.js and create new file in utils(error messages)

--- Backend/App.js
+++ Backend/App.js
@@ -4,11 +4,12 @@ import router_login from "./controller/controller_login.js";
 import path from "path";
 import { fileURLToPath } from "url";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 
-// //middleware
+//middleware
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -17,10 +18,10 @@ const path_1 = path.resolve(__dirname, "..", "Frontend", "build");
 app.use(express.static(path_1));
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 
-
-// //routes
+//routes
 app.use("/api/v1/user", router_user);
 app.use("/api/v1/login", router_login)
 
---
+++ Backend/utils/constants.js
@@ -0,0 +1,11 @@
+export const ERROR_MESSAGES = {
+    EMPTY_USERNAME: "Please enter a username",
+    EMPTY_PASSWORD: "Please enter a password",
+    EMPTY_TOKEN: "Token not find",
+    USER_NOT_FOUND: "User not found",
+    USER_UNAUTHORIZED: "UNAUTHORIZED",
+    INVALID_PASSWORD: "Invalid password",
+    INVALID_SESSION: "Invalid session",
+    TOKEN_UNAUTHORIZED: "Session not found",
+    SESSION_TIME_OUT: "Session expired"
+};
