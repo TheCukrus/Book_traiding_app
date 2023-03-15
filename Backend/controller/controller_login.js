1bbbaabd01da67d13d0073ad4d62a0b5c5481f59
Jevgenij <sssgustavsss@gmail.com>
2023-03-15 09:51:19 +0200

    create logout in controller

--- Backend/controller/controller_login.js
+++ Backend/controller/controller_login.js
@@ -49,11 +49,13 @@ router_login.post("/", async (req, res) =>
     };
 
     //creating new session
-    const create_session = await model_authentication.create(
+    const create_session = await model_authentication.findOneAndUpdate(
+      { "username": req.body.username },
       {
         "username": req.body.username,
         "session": session
-      });
+      },
+      { new: true, upsert: true });
 
     res.set("Authorization", `Bearer ${token}`)
     // res.cookie("token", session.token)
@@ -61,54 +63,7 @@ router_login.post("/", async (req, res) =>
   }
   catch (err)
   {
-    res.status(400).json({ message: err.message })
-  }
-})
-
-//PUT endpoint for updating an existing session
-router_login.put("/", async (req, res) =>
-{
-  try
-  {
-    const { username, token } = req.body;
-
-    //Check if username and token is provided
-    if (!username)
-    {
-      return res.status(400).json({ message: ERROR_MESSAGES.EMPTY_USERNAME });
-    }
-
-    if (!token)
-    {
-      return res.status(400).json({ message: ERROR_MESSAGES.EMPTY_TOKEN });
-    }
-
-    //Find user session in database
-    const user_session = await model_authentication.findOne({ username, "session.token": token });
-
-    if (!user_session)
-    {
-      return res.status(400).json({ message: ERROR_MESSAGES.UNAUTHORIZED });
-    }
-
-    //Update session expiration date
-    const update_result = await model_authentication.updateOne(
-      { "username": username, "session.token": token },
-      { $set: { "session.expiration_date": new Date(Date.now() + 30 * 60 * 1000) } }
-    );
-
-    //If session not updated, return error
-    if (update_result.nModified === 0)
-    {
-      return res.status(400).json({ message: ERROR_MESSAGES.INVALID_SESSION })
-    }
-
-    //Return success message
-    res.status(200).json({ message: "Session updated successfully" });
-  }
-  catch (err)
-  {
-    res.status(400).json({ message: err.message });
+    res.status(500).json({ message: err.message })
   }
 })
 
@@ -117,30 +72,24 @@ router_login.delete("/", async (req, res) =>
 {
   try
   {
-    const { username, token } = req.body;
 
-    //Check if username and token are provided
-    if (!username)
-    {
-      return res.status(400).json({ message: ERROR_MESSAGES.EMPTY_USERNAME });
-    }
-    if (!token)
-    {
-      return res.status(400).json({ message: ERROR_MESSAGES.EMPTY_TOKEN });
-    }
-
+    const token = req.headers.authorization.split(" ")[1];
     //Find user session in database
-    const user_session = await model_authentication.findOne({ username, "session.token": token });
+    const user_session = await model_authentication.findOne({ "session.token": token });
 
     if (!user_session)
     {
-      return res.status(400).json({ message: ERROR_MESSAGES.UNAUTHORIZED });
+      return res.status(404).json({ message: ERROR_MESSAGES.TOKEN_UNAUTHORIZED })
     }
 
     //Clear user's session data
     const delete_session = await model_authentication.updateOne(
-      { "username": username },
-      { $unset: { "session": "" } }
+      {
+        $unset: {
+          "session": "",
+          "auth_method": ""
+        }
+      }
     );
 
     //Return success message
@@ -148,7 +97,8 @@ router_login.delete("/", async (req, res) =>
   }
   catch (err)
   {
-    res.status(400).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 });
+
 export default router_login;
