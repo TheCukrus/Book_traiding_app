fdcc0b88d07a92f6e5f3119375a1d8a6fee4a8b8
Jevgenij <sssgustavsss@gmail.com>
2023-03-15 09:52:28 +0200

    create fetch_user_data

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,54 @@
 import './App.css';
 import Navigation from './components/navigation/Navigation.jsx';
 import Adress_routes from './Adress_routes.jsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const App = () =>
 {
+  const [user, set_user] = useState({});
   const [error, set_error] = useState(false);
   const [success_message, set_success_message] = useState(false);
+  const [is_authenticated, set_is_authenticated] = useState(false);
+
+  const fetch_user_data = async () =>
+  {
+    try
+    {
+      //Get token from headers
+      const token = document.cookie.replace("token=", "");
+
+      const response = await axios.get("http://127.0.0.1:80/api/v1/user/",
+        {
+          headers: { Authorization: `Bearer ${token}` }
+        })
+
+      if (response.status !== 200)
+      {
+        set_is_authenticated(false);
+        return;
+      }
+      set_is_authenticated(true);
+      set_user(response.data)
+      console.log(response.data)
+    }
+    catch (err)
+    {
+      console.log("Login fail")
+    }
+  }
+
+  useEffect(() => { fetch_user_data() }, [])
+
   return (
     <div>
 
-      <Navigation />
+      <Navigation
+        user={user}
+        set_user={set_user}
+        is_authenticated={is_authenticated}
+        set_is_authenticated={set_is_authenticated}
+      />
 
       <Adress_routes
         error={error}
@@ -18,7 +56,6 @@ const App = () =>
         success_message={success_message}
         set_success_message={set_success_message}
       />
-
     </div>
   )
 }
