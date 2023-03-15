f90a438b60b134377e454737a36cd0898a11ffd5
Jevgenij <sssgustavsss@gmail.com>
2023-03-15 13:29:18 +0200

    improved module to convert photo to data

--- Frontend/src/modules/Convert_photo_to_string.js
+++ Frontend/src/modules/Convert_photo_to_string.js
@@ -1,12 +1,21 @@
-const convert_photo_to_string = (file) =>
-{
-    return new Promise((resolve, reject) =>
-    {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => resolve(reader.result);
-        reader.onerror = error => reject(error);
-    });
-}
 
-export default convert_photo_to_string;
+export const convert_photo_to_string = (set_input_form, param) =>
+{
+    const file_reader_1 = new FileReader();
+
+    file_reader_1.addEventListener("loadend", () =>
+    {
+        set_input_form((prev_states) => (
+            {
+                ...prev_states,
+                "profile_photo": file_reader_1.result
+            }));
+    });
+
+    file_reader_1.addEventListener("error", (err) =>
+    {
+        console.log(`Error reading file: ${err}`)
+    })
+
+    file_reader_1.readAsDataURL(param)
+}
