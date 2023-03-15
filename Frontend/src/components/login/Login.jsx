590fc975e955de2c6ff06d6775d5b7ade38e11a4
Jevgenij <sssgustavsss@gmail.com>
2023-03-15 09:54:36 +0200

    modify this component

--- Frontend/src/components/login/Login.jsx
+++ Frontend/src/components/login/Login.jsx
@@ -1,13 +1,17 @@
-import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Input from "../../modules/Input.jsx";
+import Error_message from "../../modules/messages/Error_message.jsx";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Login = (props) =>
 {
-
+    const [err_msg, set_err_msg] = useState("")
     const [input_form, set_input_form] = useState(
-        { "username": "" },
-        { "password": "" }
+        {
+            "username": "",
+            "password": ""
+        }
     )
 
     const handle_input_change = (e) =>
@@ -21,22 +25,61 @@ const Login = (props) =>
         }))
     }
 
+    const handle_submit = async (e) =>
+    {
+        e.preventDefault();
+
+        //check if username and password fields aren't empty
+        if (!input_form.username || !input_form.password)
+        {
+            props.set_error(true);
+            set_err_msg("Input Password and Username!");
+            return;
+        }
+
+        try
+        {
+            const new_session = await axios.post("http://127.0.0.1:80/api/v1/login", input_form);
+            // Store token in cookies
+            document.cookie = `token=${new_session.data.token}; path=/;`;
+
+            // Redirect user to home page after successful login
+            window.location.href = "/";
+
+        }
+        catch (err)
+        {
+            props.set_error(true);
+            set_err_msg(err.response.data.message || "Failed to login.");
+        }
+    }
+
+    //If an error message is present, set a timeout to clear it after 5 sec.
+    useEffect(() =>
+    {
+        if (props.error)
+        {
+            const time_out = setTimeout(() => props.set_error(false), 5000);
+            return () => clearTimeout(time_out);
+        }
+    }, [props.error])
+
     return (
         <div>
             <h1>Login</h1>
 
-            <form>
+            <form onSubmit={handle_submit}>
                 <Input type="text" name="username" value={input_form.username} onChange={handle_input_change} placeholder="Username" required={true} min_length={3} max_length={25} />
                 <Input type="password" name="password" value={input_form.password} onChange={handle_input_change} placeholder="Password" required={true} min_length={8} />
                 <Input type="checkbox" name="remember_me" />
 
                 <label>
-                    <input type="submit" name="submit" value="Log in" />
+                    <input type="submit" name="submit" value="Login" />
+                    <p>Forgot your password? </p>
                 </label>
             </form>
-
-            <p>Doesn't have account? Sign up</p>
-            <p>Forgot your password? </p>
+            <p>Doesn't have account? <Link to="/registration">Sign up</Link></p>
+            {props.error && <Error_message messgage={err_msg} />}
         </div >
     )
 }
