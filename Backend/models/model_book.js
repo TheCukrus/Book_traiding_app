de4e7b79c25604fcb5d4e95c8cb61f4768ea88e4
Jevgenij <sssgustavsss@gmail.com>
2023-03-16 09:06:28 +0200

    create new model

---
+++ Backend/models/model_book.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import config from "../utils/config";
+
+const mongoose_connection = mongoose.createConnection(config.mongoose_url);
+
+const schema_book = new mongoose.Schema(
+    {
+        "owner": { type: String, require: true },
+        "book":
+        {
+            "title": { type: String, require: true },
+            "author": { type: String, require: true },
+            "description": { type: String, default: "" },
+            "ISBN": { type: String },
+            "genre": { type: String },
+            "publisher": { type: String },
+            "publication_year": { type: String },
+            "language": { type: String, require: true },
+            "image": { type: String, require: true },
+        }
+    }
+)
+
+const model_book = mongoose_connection.model("book", schema_book, "book");
+
+export default model_book;
