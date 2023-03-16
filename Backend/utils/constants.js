53526879cd26982c31793dfa2cfa5b07d5d6019a
Jevgenij <sssgustavsss@gmail.com>
2023-03-16 09:09:24 +0200

    create new object

--- Backend/utils/constants.js
+++ Backend/utils/constants.js
@@ -9,5 +9,25 @@ export const ERROR_MESSAGES = {
     TOKEN_UNAUTHORIZED: "Session not found",
     SESSION_TIME_OUT: "Session expired",
     NOT_MODIFIED: "Can't update session",
-    SERVER_ERROR: "Can't reach server"
+    SERVER_ERROR: "Can't reach server",
+    MISSING_FIELDS: "Fill all fields",
+    INVALID_ISBN: "ISBN is invalid",
+    USER_ALREADY_EXISTS: "This username is already taken.",
+    PHONE_NUMBER_IS_TAKEN: "This phone number is used by onother user.",
+    INVALID_PHONE_NUMBER: "Please input correct phone number",
+    EMAIL_TAKEN: "This email is taken.",
+    INVALID_EMAIL: "Please input correct email adress.",
+    INVALID_PASSWORD_REQUIRMENTS: "Password should have minimum 8 characters, at least one uppercase letter, one lowercase letter, one number and one special character",
+    BOOKS_NOT_FOUND: "No books found",
+    INTERNAL_SERVER: "Server_error",
 };
+
+
+export const SUCCESS_MESSAGES = {
+    BOOK_CREATED: "Added new book",
+    LOGOUT: "Logged out successfully",
+    NEW_USER: "New user created",
+    BOOK_UPDATED: "The book updated successfull",
+    BOOK_REMOVED: "The book removed successfull",
+
+}
