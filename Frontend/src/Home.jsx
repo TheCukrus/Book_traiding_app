3b40e975ed6822a943f0c032d7201adb1a44fe42
Jevgenij <sssgustavsss@gmail.com>
2023-03-17 23:16:04 +0200

    modify component

--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Create_book from "./components/book/Create_book.jsx";
 
 const Home = () =>
 {
     return (
         <div>
             <h1>this is home component</h1>
+            <Create_book />
         </div>
     )
 }
