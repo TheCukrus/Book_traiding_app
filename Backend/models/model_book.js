d0a4d769dc29b641d143a9f27f93b641c403e527
Jevgenij <sssgustavsss@gmail.com>
2023-03-18 22:39:02 +0200

    modify model

--- Backend/models/model_book.js
+++ Backend/models/model_book.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import config from "../utils/config";
+import config from "../utils/config.js";
 
 const mongoose_connection = mongoose.createConnection(config.mongoose_url);
 
