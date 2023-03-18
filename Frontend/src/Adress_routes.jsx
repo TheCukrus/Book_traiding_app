6e662559ca026716c058f37e61992c1bd4f29189
Jevgenij <sssgustavsss@gmail.com>
2023-03-18 22:41:32 +0200

    modify component

--- Frontend/src/Adress_routes.jsx
+++ Frontend/src/Adress_routes.jsx
@@ -2,14 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Home.jsx";
 import Registration from "./components/registration/Registration.jsx";
 import Login from "./components/login/Login.jsx";
+import Create_book from "./components/book/Create_book.jsx";
 
-const Adress_routes = ({ error, set_error, success_message, set_success_message }) =>
+const Adress_routes = ({ user, error, set_error, success_message, set_success_message }) =>
 {
     return (
         <Routes>
             <Route exact path="/" element={<Home error={error} set_error={set_error} success_message={success_message} set_success_message={set_success_message} />} />
             <Route path="/registration" element={<Registration error={error} set_error={set_error} success_message={success_message} set_success_message={set_success_message} />} />
             <Route path="/login" element={<Login error={error} set_error={set_error} success_message={success_message} set_success_message={set_success_message} />} />
+            <Route path="/create_new_book" element={<Create_book user={user} error={error} set_error={set_error} success_message={success_message} set_success_message={set_success_message} />} />
         </Routes>
     )
 }
