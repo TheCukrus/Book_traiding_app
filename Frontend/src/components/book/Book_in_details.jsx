ab9061d3bb2e819fb96697631bdf10e6e7877a9b
Jevgenij <sssgustavsss@gmail.com>
2023-03-18 22:44:08 +0200

    create new component

---
+++ Frontend/src/components/book/Book_in_details.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Book_in_detail = () =>
+{
+    return (
+        <div>
+            <h1>There would be book in detail</h1>
+        </div>
+    )
+}
+
+export default Book_in_detail;
