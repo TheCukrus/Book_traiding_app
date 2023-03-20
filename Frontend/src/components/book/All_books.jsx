ead3a201bc2e6ee793396f570471b556065bd388
Jevgenij <sssgustavsss@gmail.com>
2023-03-20 22:10:16 +0200

    making book cards

--- Frontend/src/components/book/All_books.jsx
+++ Frontend/src/components/book/All_books.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import s from "./Book.module.css";
 
 const All_books = () =>
 {
@@ -19,12 +20,35 @@ const All_books = () =>
             console.log(err);
         }
     }
-
     useEffect(() => { fetch_all_books() }, [])
 
     return (
         <div>
             <h1>There we render all books</h1>
+
+            {all_books.length === 0 ?
+
+                <p>Loading</p> :
+
+                <div className={s.card_container}>
+                    <div className={s.card_img}>
+                        <img src={all_books[0].book.image} alt={"img"} />
+                    </div>
+
+                    <div>
+                        <p>There will be rating system</p>
+                    </div>
+
+                    <div>
+                        <h2>{all_books[0].book.title}</h2>
+                        <p>{all_books[0].book.author}</p>
+                    </div>
+
+                    <div>
+                        <p>{all_books[0].book.description}</p>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
