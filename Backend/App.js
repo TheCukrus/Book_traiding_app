a016957ef5514b69359feae86b05ef5a0a9acc18
Jevgenij <sssgustavsss@gmail.com>
2023-03-26 23:57:01 +0300

    modify app that imroves limits

--- Backend/App.js
+++ Backend/App.js
@@ -6,6 +6,7 @@ import { fileURLToPath } from "url";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import router_book from "./controller/controller_book.js";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -17,9 +18,11 @@ const __dirname = path.dirname(__filename);
 const path_1 = path.resolve(__dirname, "..", "Frontend", "build");
 
 app.use(express.static(path_1));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
-app.use(cors());
 app.use(cookieParser());
+app.use(cors());
 
 //routes
 app.use("/api/v1/user", router_user);
