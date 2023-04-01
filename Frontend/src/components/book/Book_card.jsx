8ba79046505260268a1a7423296181cac18ec426
Jevgenij <sssgustavsss@gmail.com>
2023-04-01 23:49:57 +0300

    modify Book_card component that you can click on it

--- Frontend/src/components/book/Book_card.jsx
+++ Frontend/src/components/book/Book_card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import s from "./Book.module.css";
 
-const Book_card = ({ image, title, author, rating, description }) =>
+const Book_card = ({ image, title, author, rating }) =>
 {
     return (
         <div>
