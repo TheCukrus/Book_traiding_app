5ea9cee4d654c2ed149b0034dc46fdc6079ed486
Jevgenij <sssgustavsss@gmail.com>
2023-04-02 21:38:30 +0300

    create whitelist component

---
+++ Frontend/src/components/book/Whitelist.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Whitelist = () =>
+{
+    return (
+        <div>
+            <h1>This is your whitelist</h1>
+        </div>
+    )
+}
+
+export default Whitelist;
