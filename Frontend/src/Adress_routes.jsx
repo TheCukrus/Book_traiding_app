dfd4fec4715d5a99bc5b5edf99a19168c3756561
Jevgenij <sssgustavsss@gmail.com>
2023-04-02 21:36:59 +0300

    add new routes

--- Frontend/src/Adress_routes.jsx
+++ Frontend/src/Adress_routes.jsx
@@ -5,6 +5,8 @@ import Login from "./components/login/Login.jsx";
 import Create_book from "./components/book/Create_book.jsx";
 import Book_in_details from "./components/book/Book_in_details.jsx";
 import Profile_page from "./components/profile/Profile_page.jsx";
+import My_books from "./components/book/My_books.jsx";
+import Whitelist from "./components/book/Whitelist.jsx";
 
 const Adress_routes = ({ user, error, set_error, success_message, set_success_message, all_books, set_books_id }) =>
 {
@@ -16,6 +18,8 @@ const Adress_routes = ({ user, error, set_error, success_message, set_success_me
             <Route path="/create_new_book" element={<Create_book user={user} error={error} set_error={set_error} success_message={success_message} set_success_message={set_success_message} />} />
             <Route path="/book/:id" element={<Book_in_details />} />
             <Route path="/user/:username" element={<Profile_page user={user} />} />
+            <Route path="/user/:username/my_books" element={<My_books user={user} set_books_id={set_books_id} />} />
+            <Route path="/user/:username/whitelist" element={<Whitelist user={user} />} />
         </Routes>
     )
 }
