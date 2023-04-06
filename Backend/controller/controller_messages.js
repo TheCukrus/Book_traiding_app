d55dfba38720099c39704b0d516ae327cd805524
Jevgenij <sssgustavsss@gmail.com>
2023-04-06 23:39:09 +0300

    update router

--- Backend/controller/controller_messages.js
+++ Backend/controller/controller_messages.js
@@ -47,24 +47,131 @@ router_messages.post("/", check_session, update_session, async (req, res) =>
     }
 })
 
-export default router_messages;
-/*
-Retrieving all chats for a particular user
-Retrieving all messages for a particular chat
-Adding a new message to a particular chat
-Updating the unread message count for a particular chat
-Updating the timestamp of the last message sent for a particular chat
+//GET method to retriev all chats for a particular user
+router_messages.get("/chats", check_session, update_session, async (req, res) => 
+{
+    try
+    {
+        //Retrieve all chats that involve the user
+        const chats = await model_chat.find({}).populate("users", "username");
+        res.status(200).json({ chats });
+    }
+    catch (err)
+    {
+        console.log(err);
+        res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER });
+    }
+})
 
-    "users": [ { type: { type: String },required: true }],
-    "messages":
-        [
-            {
-                "sender_ID": { type: String, required: true },
-                "receive_ID": { type: String, required: true },
-                "content": { type: String, required: true },
-                "timestamp": { type: Date, default: Date.now }
-            }
-        ],
-    "last_message_timestamp":{ type: Date, default: null },
-    "unread_count": { type: Number, default: 0 }
-    */
+//GET method to retrieving all messages for a particular chat
+router_messages.get("/:chat_id", check_session, update_session, async (req, res) =>
+{
+    try
+    {
+        const { chat_id } = req.params;
+
+        //Check if the chat exists
+        const chat = await model_chat.findById(chat_id);
+        if (!chat)
+        {
+            res.status(404).json({ message: ERROR_MESSAGES.CHAT_NOT_EXISTS })
+        }
+
+        //Retrieve all messages for the chat and populate the sender and receiver IDs with their usernames
+        const messages = await model_chat.findById(chat_id).populate("messages.sender_ID", "username").populate("messages.receive_ID", "username");
+
+        res.status(200).json({ messages })
+
+    }
+    catch (err)
+    {
+        console.log(err);
+        res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER })
+    }
+})
+
+//POST method adding a new message to a particular chat
+router_messages.post("/:chat_id", check_session, update_session, async (req, res) =>
+{
+    try
+    {
+        const { chat_id } = req.params;
+        const { sender_ID, receive_ID, content } = req.body;
+
+        //Check if chat exists
+        const chat = await model_chat.findById(chat_id);
+        if (!chat)
+        {
+            return res.status(404).json({ message: ERROR_MESSAGES.CHAT_NOT_EXISTS })
+        }
+
+        //Add a new message to the chat
+        chat.messages.push({ sender_ID, receive_ID, content });
+        chat.last_message_timestamp = new Date();
+        chat.unread_count += 1;
+        await chat.save();
+
+        res.status(200).json({ message: SUCCESS_MESSAGES.MESSAGE_SENT })
+    }
+    catch (err)
+    {
+        console.log(err);
+        res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER })
+    }
+})
+
+//PUT method for updating the unread message count for a particular chat
+router_messages.put("/:chat_id", check_session, update_session, async (req, res) =>
+{
+    try
+    {
+        const { chat_id } = req.params;
+
+        //Check if chat exists 
+        const chat = await model_chat.findById(chat_id);
+        if (!chat)
+        {
+            return res.status(404).json({ message: ERROR_MESSAGES.CHAT_NOT_EXISTS })
+        }
+
+        //Update the unread message count to 0 for the chat
+        chat.unread_count = 0;
+        await chat.save();
+
+        res.status(200).json({ message: SUCCESS_MESSAGES.UNREAD_COUNT_UPDATED })
+    }
+    catch (err)
+    {
+        console.log(err);
+        res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER })
+    }
+})
+
+//PUT method to update the timestamp of the last message send for a particular chat
+router_messages.put("/:chat_id/last-message-timestamp", check_session, update_session, async (req, res) =>
+{
+    try
+    {
+        const { chat_id } = req.params;
+
+        //check if chat exists
+        const chat = await model_chat.findById(chat_id);
+        if (!chat)
+        {
+            return res.status(404).json({ message: ERROR_MESSAGES.CHAT_NOT_EXISTS })
+        }
+
+        //Update the last message timestamp for the chat to the current time
+        chat.last_message_timestamp = new Date();
+        await chat.save();
+
+        res.status(200).json({ message: SUCCESS_MESSAGES.LAST_MESSAGE_TIMESTAMP })
+    }
+    catch (err)
+    {
+        console.log(err)
+        res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER })
+    }
+})
+
+export default router_messages;
