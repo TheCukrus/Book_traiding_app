17c33a9b75f2221697ad5356059cbcf14e67ca8f
Jevgenij <sssgustavsss@gmail.com>
2023-04-08 08:57:43 +0300

    fixed bug

--- Backend/models/model_messages.js
+++ Backend/models/model_messages.js
@@ -3,19 +3,21 @@ import config from "../utils/config.js";
 
 const mongoose_connection = mongoose.createConnection(config.mongoose_url);
 
-const schema_messages = new mongoose.Schema({
-    "sender_ID": { type: String, required: true },
-    "receive_ID": { type: String, required: true },
-    "content": { type: String, required: true },
-    "timestamp": { type: Date, default: Date.now }
-});
-
-const schema_chat = new mongoose.Schema({
-    "users": [{ type: { type: String }, required: true }],
-    "messages": [schema_messages],
-    "last_message_timestamp": { type: Date, default: null },
-    "unread_count": { type: Number, default: 0 }
-});
+const schema_chat = new mongoose.Schema(
+    {
+        "users": [{ type: { type: String, required: true } }],
+        "last_message_timestamp": { type: Date, default: null },
+        "unread_count": { type: Number, default: 0 },
+        "messages":
+            [
+                {
+                    "sender_ID": { type: String, required: true },
+                    "receive_ID": { type: String, required: true },
+                    "content": { type: String, required: true },
+                    "timestamp": { type: Date, default: Date.now }
+                }
+            ]
+    });
 
 const model_chat = mongoose_connection.model("chat", schema_chat);
 
