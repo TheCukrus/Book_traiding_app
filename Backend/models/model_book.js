a880cab38ae8b56e9630c120dbdd3efe37bf629a
Jevgenij <sssgustavsss@gmail.com>
2023-04-08 08:57:22 +0300

    improved model

--- Backend/models/model_book.js
+++ Backend/models/model_book.js
@@ -5,18 +5,18 @@ const mongoose_connection = mongoose.createConnection(config.mongoose_url);
 
 const schema_book = new mongoose.Schema(
     {
-        "owner": { type: String, require: true },
+        "owner": { type: String, required: true },
         "book":
         {
-            "title": { type: String, require: true },
-            "author": { type: String, require: true },
+            "title": { type: String, required: true },
+            "author": { type: String, required: true },
             "description": { type: String, default: "" },
             "ISBN": { type: String },
             "genre": { type: String },
             "publisher": { type: String },
             "publication_year": { type: String },
-            "language": { type: String, require: true },
-            "image": { type: String, require: true },
+            "language": { type: String, required: true },
+            "image": { type: String, required: true },
             "price": { type: String },
         },
         "created_at": { type: Date, default: Date.now },
