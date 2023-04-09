ca19164ee037bbf665e1b077a547476cb22dfd09
Jevgenij <sssgustavsss@gmail.com>
2023-04-09 13:20:47 +0300

    modify model

--- Backend/models/model_user_rating.js
+++ Backend/models/model_user_rating.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const schema_rating = new mongoose.Schema(
     {
-        "user_id": { type: mongoose.Schema.Types.ObjectId, ref: "User", require: true },
+        "user_id": { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
         "rating": { type: Number, require: true },
         "created_at": { type: Date, default: Date.now }
     }
