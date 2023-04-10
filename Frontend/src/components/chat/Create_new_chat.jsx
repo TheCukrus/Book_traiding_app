9ca29b7d8ec59f423734e1b22f409df6317431a1
Jevgenij <sssgustavsss@gmail.com>
2023-04-10 23:32:43 +0300

    create new chat component

---
+++ Frontend/src/components/chat/Create_new_chat.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Create_new_chat = () =>
+{
+    return (
+        <div>
+            <h1>There you can create a new chat!</h1>
+        </div>
+    )
+}
+
+export default Create_new_chat;
