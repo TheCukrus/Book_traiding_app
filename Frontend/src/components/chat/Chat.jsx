e794fb4cb7a1e1d5d9305e436b2d992d6a08f9e5
Jevgenij <sssgustavsss@gmail.com>
2023-04-10 23:32:18 +0300

    create main chat component

---
+++ Frontend/src/components/chat/Chat.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Create_new_chat from "./Create_new_chat.jsx";
+
+const Chat = () =>
+{
+    return (
+        <div>
+            <h1>There is the chat component</h1>
+
+            <Create_new_chat />
+        </div>
+    )
+}
+
+export default Chat;
